0a95cc1fd3a096285697ac1708747ef2974b255d
solehpolysas <solehpolysas@gmail.com>
2019-03-17 10:38:21 +0800

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,7 +1,7 @@
-version=0.8.9
+version=0.8.12
 apt-get update && apt-get -y upgrade
 apt-get install gcc make git -y
-wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/SnoyIatK/3proxy/raw/master/archive/3proxy-${version}.tar.gz
+wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/solehpolysas/3proxy/raw/master/archive/3proxy-${version}.tar.gz
 tar xzf 3proxy-${version}.tar.gz
 cd 3proxy-${version}
 make -f Makefile.Linux
@@ -9,12 +9,12 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/solehpolysas/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 mkdir /var/log/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/.proxyauth
+wget --no-check-certificate https://github.com/solehpolysas/3proxy/raw/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate  https://raw.github.com/SnoyIatk/3proxy/master/3proxy
+wget --no-check-certificate  https://raw.github.com/solehpolysas/3proxy/master/3proxy
 chmod  +x /etc/init.d/3proxy
 update-rc.d 3proxy defaults
